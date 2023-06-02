e960ba9e807be817621e42cf3caedab0872b1a29
luciafava <artgallery077@gmail.com>
2023-06-02 19:04:25 +0200

    feature: optional start value

---
+++ source/grange.js
@@ -0,0 +1,29 @@
+function* grange (start, end, transform, options) {
+    if (typeof end !== 'number') {
+      [ options, transform, end, start ] =
+        [ transform, end, start, 0];
+    }
+  
+    const shouldTransform = typeof transform === 'function';
+    if (!shouldTransform) options = transform;
+  
+    const {
+      step = 1,
+      loop
+    } = options || {};
+  
+    if (start > end) {
+      for (let i = start; i >= end; i -= step) {
+        yield shouldTransform ? transform(i) : i;
+        if (i <= end && loop === true) i = start + step;
+      }
+      return;
+    }
+  
+    for (let i = start; i <= end; i += step) {
+      yield shouldTransform ? transform(i) : i;
+      if (i >= end && loop === true) i = start - step;
+    }
+  }
+  
+  module.exports = grange;
